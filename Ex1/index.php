d7892370eb3c212cc58f87aba779ee6d6cd068da
Tienluc037 <nguyentienluc12a3@gmail.com>
2022-01-26 09:13:18 +0700

    array

--- Ex1/index.php
+++ Ex1/index.php
@@ -9,4 +9,36 @@ function createArr($number) {
     return $arr;
 }
 
-print_r(createArr(5));
+//print_r(createArr(5));
+
+
+//echo "<hr>";
+
+//Sắp xếp arr theo thứ tự tăng dần.
+
+$arr = createArr(5);
+echo "<pre>Tạo mảng ngẫu nhiên ";
+print_r($arr);
+
+function sortArray($arr)
+{
+    for ($i = 0; $i < count($arr); $i++) {
+        $min = $arr[$i];
+        $index = $i;
+        for ($j = $i + 1; $j < count($arr); $j++) {
+            if ($min > $arr[$j]) {
+                $min = $arr[$j];
+                $index = $j;
+            }
+        }
+        $temp = $arr[$i];
+        $arr[$i] = $min;
+        $arr[$index] = $temp;
+    }
+    return $arr;
+
+}
+
+$arr = sortArray($arr);
+echo "<pre><hr> Sắp xếp mảng tăng dần";
+print_r($arr);
