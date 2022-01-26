aa649dc336d6c82957ccc64c91c7d038d835d504
Tienluc037 <nguyentienluc12a3@gmail.com>
2022-01-26 11:45:15 +0700

    update

---
+++ Ex2/số dương chẵn lớn nhất/index.php
@@ -0,0 +1,17 @@
+<?php
+//Tìm số dương chẵn lớn nhất trong mảng.
+
+function searchMax($arr){
+    $max = $arr[0];
+    for ($i = 1; $i < count($arr); $i++ ) {
+        if($max < $arr[$i] && $arr[$i] > 0 && $arr[$i] % 2 == 0) {
+            $max = $arr[$i];
+        }
+    }
+    return $max;
+}
+$arr = [1,5,6,7,8,-4,-1];
+echo "<pre>";
+print_r($arr);
+echo "Số chẵn lớn nhất là :" . searchMax($arr)."<hr>";
+
