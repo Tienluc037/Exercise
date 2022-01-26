a8ed8d2939e28ded2a58210510ec13202cca4647
Tienluc037 <nguyentienluc12a3@gmail.com>
2022-01-26 11:17:20 +0700

    update

--- Ex2-max-min(số thực)/index.php
+++ Ex2-max-min(số thực)/index.php
@@ -1,4 +1,7 @@
 <?php
+
+//Nhập một mảng gồm N số thực.
+
 function createFloatArr($number) {
     $arr = [];
     for ($i = 0; $i < $number ; $i++) {
@@ -10,3 +13,22 @@ function createFloatArr($number) {
 $arr = createFloatArr(5);
 echo "<pre>Tạo mảng số thực ngẫu nhiên ";
 print_r($arr);
+
+
+//Tìm số lớn nhất trong mảng.
+function searchMax($arr)
+{
+    $max = $arr[0];
+    for ($i = 1; $i < count($arr); $i++) {
+        if ($arr[$i] > $max) {
+            $max = $arr[$i];
+        }
+    }
+    return $max;
+}
+
+$arr = searchMax($arr);
+echo "<pre><hr>Số lớn nhất trong mảng là: ";
+print_r($arr);
+
+
